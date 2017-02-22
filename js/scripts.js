7202fcf0c67ff426bb8ff8b49f895e01e97d4b85
vladimir <vovmaster2@gmail.com>
2017-02-22 08:01:49 +0200

    update

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+	
+	// $('.slider').slick({
+	//   dots: true,
+	//   infinite: true,
+	//   speed: 500
+	// });
+	var timeoutID = 0;
+	
+	$('.slider-wrap-page').each(function() {
+		var thisSlide = $(this);
+		var oneSlide = thisSlide.find('.one-slide');
+		var oneSlideLength = oneSlide.length;
+
+		$('.slider-wrap-page .slider').slick({
+			dots: true,
+			infinite: true,
+			speed: 500,
+			fade: true,
+			slidesToShow: 1,
+			adaptiveHeight: true,
+		});
+
+		$('#slider-slick-control').slider({
+			step: 1,
+			value: 0,
+			min: 0,
+			max: oneSlideLength - 1,
+			// create: function() {
+			// 	handle.text( $( this ).slider( "value" ) );
+			// },
+			slide: function( event, ui ) {
+				// console.log( ui.value );
+
+				var activeSlide = ui.value + 1;
+
+				clearTimeout(timeoutID);
+				timeoutID = setTimeout(function() {
+					thisSlide.find('.slick-dots > *:nth-child(' + activeSlide + ')').click();
+				}, 300);
+				
+			}
+		});	
+
+	});
+
+
+	
+
+
+
+});
